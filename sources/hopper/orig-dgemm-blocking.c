96815b67ced8bfee8543066bf6ddfeb3c9205448
crashh <lthomasen@gmail.com>
2015-09-28 12:02:11 +0200

    Reverted to transposed version

--- sources/hopper/orig-dgemm-blocking.c
+++ sources/hopper/orig-dgemm-blocking.c
@@ -37,7 +37,7 @@ void basic_dgemm( int lda, int M, int N, int K,
             double cij = C[i+j*lda];
             #pragma GCC ivdep
             for( int k = 0; k < K; k++ )
-                 cij += A[i+k*lda] * B[k+j*lda];
+                 cij += A[k+i*lda] * B[k+j*lda];
             C[i+j*lda] = cij;
        }
 }
@@ -52,7 +52,7 @@ void simd_dgemm( int lda, int M, int N, int K,
 		{
 			vRes = _mm_setzero_pd();
 			for (int k = 0; k < K; k+=2) {
-				v1 = _mm_loadu_pd(&A[i+k * lda]);
+				v1 = _mm_loadu_pd(&A[k+i * lda]);
 				v2 = _mm_loadu_pd(&B[k+j * lda]);
 				vMul = _mm_mul_pd(v1, v2);
 
@@ -92,7 +92,7 @@ void do_block( int lda, double *A, double *B, double *C,
        Each iteration here, is iterating the missing indexes of the
        ones we skipped in square_dgemm. 
      */
-     if (K%2 != 0)
+     if (true)
         basic_dgemm( lda, M, N, K, A + k + i*lda, B + k + j*lda, C + i + j*lda);
      else
         simd_dgemm( lda, M, N, K, A + k + i*lda, B + k + j*lda, C + i + j*lda);
@@ -103,18 +103,18 @@ void square_dgemm( int M, double *A, double *B, double *C )
     // Create transpose, this costs us some, but makes up in time
     // for bigger matrices. Note that this required a small change in
     // basic_dgemm when accessing the transposed matrix.
-    /*int n = M; 
+    int n = M; 
 	double tmp[n*n];
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             //Save transpose in tmp
 			tmp[i+j*n] = A[j+i*n]; 
 		}
-	}*/
+	}
     // Now we do the original code with the transposed matrix in place of A.
     // A has to be the one transposed since the given matrices are column-major.
     for( int i = 0; i < M; i += BLOCK_SIZE )
         for( int j = 0; j < M; j += BLOCK_SIZE )
             for( int k = 0; k < M; k += BLOCK_SIZE )
-                do_block( M, A, B, C, i, j, k );
+                do_block( M, tmp, B, C, i, j, k );
 }
