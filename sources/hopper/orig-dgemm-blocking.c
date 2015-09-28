2102f59b5c2e31ebe726bc200a77b890938519c0
crashh <lthomasen@gmail.com>
2015-09-28 17:40:42 +0200

    Removed the unaligned load, probably breaking it.

--- sources/hopper/orig-dgemm-blocking.c
+++ sources/hopper/orig-dgemm-blocking.c
@@ -49,8 +49,8 @@ void simd_dgemm(int lda, int M, int N, int K,
             double cij[2] __attribute__ ((aligned (16))) = {C[i+j*lda], 0};
             vRes = _mm_load_pd(cij);
             for (int k = 0; k < K; k += 2) {
-                v1 = _mm_loadu_pd(&A[k + i * lda]);
-                v2 = _mm_loadu_pd(&B[k + j * lda]);
+                v1 = _mm_load_pd(&A[k + i * lda]);
+                v2 = _mm_load_pd(&B[k + j * lda]);
                 vMul = _mm_mul_pd(v1, v2);
 
                 vRes = _mm_add_pd(vRes, vMul);
