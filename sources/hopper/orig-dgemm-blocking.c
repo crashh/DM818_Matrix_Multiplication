7b2ef0bd2b8831d1d3cf4c90fe1f17c83da1ca95
crashh <lthomasen@gmail.com>
2015-09-28 21:35:35 +0200

    Reverted to working state.

--- sources/hopper/orig-dgemm-blocking.c
+++ sources/hopper/orig-dgemm-blocking.c
@@ -34,35 +34,22 @@ void basic_dgemm(int lda, int M, int N, int K, double *A, double *B, double *C) 
             double cij = C[i + j * lda];
             #pragma GCC ivdep
             for( int k = 0; k < K; k++ )
-                 cij += A[i+k*lda] * B[k+j*lda];
+                 cij += A[k+i*lda] * B[k+j*lda];
             C[i+j*lda] = cij;
         }
     }
 }
 
 void simd_dgemm(int lda, int M, int N, int K,
-                double *A, double *B, double *C) {                
-    // Create transpose:
-    int idx = 0;
-	double tmp[M*N];
-    for (int i = 0; i < M; ++i) {
-        for (int j = 0; j < N; ++j) {
-            //Save transpose in tmp
-			tmp[idx++] = A[j+i*lda]; 
-		}
-	}
-	
-	// Define 128bit registers:
-    __m128d v1, v2, vMul, vRes;     
+                double *A, double *B, double *C) {
+    __m128d v1, v2, vMul, vRes; // Define 128bit registers.    
 
-    idx=0;
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
             double cij[2] __attribute__ ((aligned (16))) = {C[i+j*lda], 0};
             vRes = _mm_load_pd(cij);
             for (int k = 0; k < K; k += 2) {
-                v1 = _mm_loadu_pd(tmp[idx]);
-                idx+=2;
+                v1 = _mm_loadu_pd(&A[k + i * lda]);
                 v2 = _mm_loadu_pd(&B[k + j * lda]);
                 vMul = _mm_mul_pd(v1, v2);
 
@@ -102,13 +89,22 @@ void do_block(int lda, double *A, double *B, double *C, int i, int j, int k) {
     }
 }
 
-void square_dgemm(int M, double *A, double *B, double *C) {	
+void square_dgemm(int M, double *A, double *B, double *C) {
+    // Create transpose:
+    // This should be moved, to inner loop.
+	double tmp[M*M];
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < M; ++j) {
+			tmp[i+j*M] = A[j+i*M]; 
+		}
+	}
+	
     // Now we do the original code with the transposed matrix in place of A.
     // A has to be the one transposed since the given matrices are column-major.
     for (int i = 0; i < M; i += BLOCK_SIZE) {
         for (int j = 0; j < M; j += BLOCK_SIZE) {
             for (int k = 0; k < M; k += BLOCK_SIZE) {
-                do_block(M, A, B, C, i, j, k);
+                do_block(M, tmp, B, C, i, j, k);
             }
         }
     }
