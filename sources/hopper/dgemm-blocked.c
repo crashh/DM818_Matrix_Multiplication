eb158f1288b81f4e37e6dfc6808ded19610459cb
crashh <lthomasen@gmail.com>
2015-10-01 15:33:31 +0200

    Why u not work

--- sources/hopper/dgemm-blocked.c
+++ sources/hopper/dgemm-blocked.c
@@ -11,7 +11,7 @@
 const char *dgemm_desc = "Simple blocked dgemm.";
 
 #ifndef BLOCK_SIZE
-#define BLOCK_SIZE 38
+#define BLOCK_SIZE 32
 #endif
 
 #define min(a, b) (((a)<(b))?(a):(b))
@@ -64,7 +64,7 @@ void simd_dgemm(int lda, int M, int N, int K,
             const double cij[2] __attribute__ ((aligned (16))) = {C[i+j*lda], 0};
             vRes = _mm_load_pd(cij);
             for (int k = 0; k < K; k += 2) {
-                v1 = _mm_load_pd(&A[i + k * K]);
+                v1 = _mm_load_pd(&aPacked[k + i * K]);
                 v2 = _mm_load_pd(&bPacked[k + j * K]);
                 vMul = _mm_mul_pd(v1, v2);
 
