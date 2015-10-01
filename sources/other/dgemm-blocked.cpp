85f02eca094e1c06e491b34fa4cec3a8e5a4d772
crashh <lthomasen@gmail.com>
2015-10-01 22:45:09 +0200

    lol

--- sources/other/dgemm-blocked.cpp
+++ sources/other/dgemm-blocked.cpp
@@ -62,8 +62,8 @@ void simd_dgemm(int lda, int M, int N, int K,
             for (int col = 0; col < K; col++) {             // Entire column at a time.
                 aPacked[idx++] = A[col * lda + i];
             }
-            printf(aPacked[0]);
-            printf(aPacked[31]);
+            printf("arr0 %d",aPacked[0]);
+            printf("arr31 %d",aPacked[31]);
         //}
     
     
