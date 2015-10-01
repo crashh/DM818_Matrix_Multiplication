bb0a92f2a16948b13f8f5493918ed5c462607c8c
Dan Sebastian Thrane <dthrane@gmail.com>
2015-10-01 13:46:32 +0200

    Stuff

--- sources/hopper/orig-dgemm-blocking.c
+++ sources/hopper/orig-dgemm-blocking.c
@@ -41,47 +41,74 @@ void basic_dgemm(int lda, int M, int N, int K, double *A, double *B, double *C) 
 }
 
 /**
+ * A is M-by-K (height-by-width), and is stored in a row-major matrix
+ * B is K-by-N, and is stored and packed in a column-major matrix
+ * C is still stored as a column-major matrix, this is the output matrix.
+ */
+void simd_dgemm(int lda, int M /* mc */, int N /* nr */, int K /* K */, double *A, double *B, double *C) {
+    __m128d v1, v2, vMul, vRes; // Define 128bit registers.
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            double cij[2] __attribute__ ((aligned (16))) = {C[i+j*lda], 0}; // TODO assume correct, even though it isn't
+            vRes = _mm_load_pd(cij);
+            for (int k = 0; k < K; k += 2) {
+                v1 = _mm_loadu_pd(&A[k + i * K]);
+                v2 = _mm_loadu_pd(&B[k + j * K]);
+                vMul = _mm_mul_pd(v1, v2);
+
+                vRes = _mm_add_pd(vRes, vMul);
+            }
+            vRes = _mm_hadd_pd(vRes, vRes);
+            _mm_store_sd(&C[i + j * lda], vRes);
+        }
+    }
+}
+
+/**
  * A is M-by-K (height-by-width)
  * B is K-by-N
  * C is M-by-N
  */
 void blocked_gepp(int lda, int M, int N, int K, double *A, double *B, double *C) {
     int nr = 4; // The width of each B submatrix (Thus each submatrix will be nr * K)
+    int mc = 4; // The height of each A submatrix (This each submatrix will be mc * K)
 
     // Pack B into a contiguous column-major matrix
     // TODO inline and align and other performance stuff
-    double *bPacked = malloc(sizeof(double) * K * N + // allocate space for the part existing in B
-                                     sizeof(double) * ((N % nr) * K)); // allocate space for additional padding
+    int bColumns = N + (N % nr);
+    int bSize = K * bColumns;
+    double *bPacked = malloc(sizeof(double) * bSize);
     int idx = 0;
     for (int col = 0; col < N; col++) {
         for (int row = 0; row < K; row++) {
             bPacked[idx++] = B[col * lda + row];
         }
     }
-    for (int col = K * N; col < K * N + (N % nr) * K; col++) { // Padding
+    for (int col = K * N; col < bSize; col++) { // Padding
         for (int row = 0; row < K; row++) { // TODO unroll
             bPacked[idx++] = 0;
         }
     }
 
-    for (int i = 0; i < M; i += K) {
+    for (int i = 0; i < M; i += mc) {
         // Pack A[i] (i.e. the ith submatrix, not some element) into a contiguous row-major work matrix
-        double *aPacked = malloc(sizeof(double) * K * N);
+        double *aPacked = malloc(sizeof(double) * mc * K);
         idx = 0;
-        int rows = min(M, K);
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < N; col++) {
-                aPacked[idx++] = A[col * lda + row + i];
+        int maxRow = min(i * mc + mc, M);
+        for (int row = i * mc; row < maxRow; row++) {
+            for (int col = 0; col < K; col++) {
+                aPacked[idx++] = A[col * lda + row];
             }
         }
-        for (int row = M - i; row < K; row++) { // Add padding
-            for (int col = 0; col < N; col++) { // TODO Unroll
+        for (int row = maxRow; row < i * mc + mc; row++) { // Add padding
+            for (int col = 0; col < K; col++) { // TODO Unroll
                 aPacked[idx++] = 0;
             }
         }
 
         for (int j = 0; j < N; j++) {
-            // TODO Multiply A[i] * B[j]
+            simd_dgemm(lda, mc, nr, K, aPacked, bPacked + j * nr, C);
         }
     }
 }
