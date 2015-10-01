73ec57bc9d992c51ca318fe242c13806cb95a54e
crashh <lthomasen@gmail.com>
2015-10-01 22:16:34 +0200

    Error calc

--- sources/other/dgemm-blocked.cpp
+++ sources/other/dgemm-blocked.cpp
@@ -58,7 +58,7 @@ void simd_dgemm(int lda, int M, int N, int K,
         // Pack the A Matrix in parts of two:
         double aPacked[K*M] __attribute__ ((aligned(64)));
         idx = 0;
-        for (int row = i * 2; row < 2; row++) {             // 2 rows at a time.
+        for (int row = i * 1; row < row+1; row++) {         // 1 rows at a time.
             for (int col = 0; col < K; col++) {             // Entire column at a time.
                 aPacked[idx++] = A[col * lda + row];
             }
