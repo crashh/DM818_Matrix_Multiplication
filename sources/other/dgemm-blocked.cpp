e6fb85080bfbd08c6eb8af60a5a26efdc1a2a137
Lars Thomasen <latho12@login1.imada.sdu.dk>
2015-10-08 08:58:36 +0200

    Now runs linear with dips

--- sources/other/dgemm-blocked.cpp
+++ sources/other/dgemm-blocked.cpp
@@ -9,13 +9,12 @@
 #include <math.h>
 
 const char *dgemm_desc = "Simple blocked dgemm.";
-int BLOCK_SIZE;
 
-//#ifndef BLOCK_SIZE
-//#define BLOCK_SIZE 24
-//#endif
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 92
+#endif
 
-#define MC 4
+#define MC 20
 #define min(a,b) (((a)<(b))?(a):(b))
 
 /*
@@ -29,11 +28,11 @@ void simd_dgemm_4n(const int lda, const int M, const int N, const int K,
                 double *__restrict C) {
     __m128d vA, vB1, vB2, vB3, vB4, vMul, vRes1, vRes2, vRes3, vRes4; // Define 128bit registers.
     const int Kpadded = (K+(K%2));    // Adjust K length to account for padding:
-    double cAux[K*(N+4)] __attribute__ ((aligned(16)));
-    
+    double cAux[N+4] __attribute__ ((aligned(16)));
+
     // Pack the B Matrix:
     // TODO: Refactor this.
-    double bPacked[Kpadded*(N+4)] __attribute__ ((aligned(16)));
+    double bPacked[Kpadded*(N+(N%4))] __attribute__ ((aligned(16)));
     for (int col = 0; col < N; col++) {
         for (int row = 0; row < K; row++) {
             bPacked[col * Kpadded + row] = B[col * lda + row];
@@ -44,33 +43,29 @@ void simd_dgemm_4n(const int lda, const int M, const int N, const int K,
         }
     }
     // More padding to B for extra N elements:
-    for (int col = N; col < (N+4); col++) {
+    for (int col = N; col < (N+(N%4)); col++) {
         for (int row = 0; row < Kpadded; row++) {
             bPacked[col * Kpadded + row] = 0;
         }
     }
 
-    double aPacked[Kpadded*M] __attribute__ ((aligned(16)));
+    double aPacked[Kpadded*M] __attribute__ ((aligned(16))); //tmp addon
     // Add padding to A:
     for (int col = K; col < Kpadded; col++) {
         for (int row = 0; row < M; row++) {
             aPacked[col + row * Kpadded] = 0;
         }
     }
-
-    for (int i = 0; i < M; i+=MC) {
     
+    for (int i = 0; i < M; i+=MC) {
         // Pack the A Matrix::
         for (int col = 0; col < K; col++) {         // Entire column at a time.
-            for (int row = i; row < MC+i; row++) {  // mc rows at a time.
-                if (row >= M) {break;}
+            for (int row = i; row < min(M,i+MC); row++) {  // mc rows at a time.
                 aPacked[col + row * Kpadded] = A[col * lda + row];
             }
         }
-
         // Now do the calculations:    
-        for (int z = i; z < i+MC; z++) { // TODO: Should attempt to unroll this.
-            if (z >= M) {break;}
+        for (int z = i; z < min(M,i+MC); z++) {
             // Unrolling access to the B matrix, since it is accessed
             // multiple time for every element in A:
             for (int j = 0; j < N; j+=4) {
@@ -98,41 +93,38 @@ void simd_dgemm_4n(const int lda, const int M, const int N, const int K,
                 vRes2 = _mm_hadd_pd(vRes2, vRes2);
                 vRes3 = _mm_hadd_pd(vRes3, vRes3);       
                 vRes4 = _mm_hadd_pd(vRes4, vRes4);           
-                _mm_store_sd(&cAux[z + j * K], vRes1);
-                _mm_store_sd(&cAux[z + (j+1) * K], vRes2);             
-                _mm_store_sd(&cAux[z + (j+2) * K], vRes3);
-                _mm_store_sd(&cAux[z + (j+3) * K], vRes4);
-                // Ide: Smid resultater i en tmp C og til sidst smid 
-                // for N og M over i korrekt C matrix. (Overskydende kommer
-                // således ikke med over).
+                _mm_store_sd(&cAux[j], vRes1);
+                _mm_store_sd(&cAux[(j+1)], vRes2);             
+                _mm_store_sd(&cAux[(j+2)], vRes3);
+                _mm_store_sd(&cAux[(j+3)], vRes4);
             }
+        // Ide: Smid resultater i en tmp C og til sidst smid 
+        // for N og M over i korrekt C matrix. (Overskydende kommer
+        // således ikke med over).
+        // Move from aux matrix to result matrix:
+        for (int j = 0; j < N; j++) {
+             C[z+j*lda] = cAux[j];
+        }
 	    }
     }
-    // Move from aux matrix to result matrix:
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            C[i+j*lda] = cAux[i+j*K];
-        }
-    }
 }
 
-void do_block(int lda, double *A, double *B, double *C, int i, int j, int k) {
-    int M = min(BLOCK_SIZE, lda - i);
-    int N = min(BLOCK_SIZE, lda - j);
-    int K = k;
-    simd_dgemm_4n(lda, M, N, K, A + i, B + j * lda, C + i + j * lda);
-}
-
-void square_dgemm( int M, double *A, double *B, double *C )
+void square_dgemm (int lda, double* A, double* B, double* C)
 {
+  /* For each block-row of A */ 
+  for (int i = 0; i < lda; i += BLOCK_SIZE)
+    /* For each block-column of B */
+    for (int j = 0; j < lda; j += BLOCK_SIZE)
+      /* Accumulate block dgemms into block of C */
+      for (int k = 0; k < lda; k += BLOCK_SIZE)
+      {
+	    /* Correct block dimensions if block "goes off edge of" the matrix */
+	    int M = min(BLOCK_SIZE, lda-i);
+	    int N = min(BLOCK_SIZE, lda-j);
+	    int K = min(BLOCK_SIZE, lda-k);
 
-     if(M<192) {BLOCK_SIZE = 100;}
-     else if (M<319) {BLOCK_SIZE = 72;}
-     else if (M<500) {BLOCK_SIZE = 60;}
-     else if (M<650) {BLOCK_SIZE = 40;}
-     else {BLOCK_SIZE = 36;}
-
-     for( int i = 0; i < M; i += BLOCK_SIZE )
-        for( int j = 0; j < M; j += BLOCK_SIZE )
-            do_block( M, A, B, C, i, j, M );
+	    /* Perform individual block dgemm */
+        simd_dgemm_4n(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);	    
+        //do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+      }
 }
